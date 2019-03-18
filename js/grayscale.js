b5137b819bd0c06086a9c4bfad95fa12a6fd154b
IshMSahni <ishsahni@hotmail.com>
2019-03-18 06:37:51 -0400

    update

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,33 +38,3 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
-   // Magnific popup calls
-  $('#portfolio').magnificPopup({
-      delegate: 'a',
-      type: 'image',
-      tLoading: 'Loading image #%curr%...',
-      mainClass: 'mfp-img-mobile',
-      gallery: {
-        enabled: true,
-        navigateByImgClick: true,
-        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-      },
-      image: {
-        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-        /*titleSrc: function(item) {
-          return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-        }*/
-      },
-      callbacks: {
-        elementParse: function(item) {
-     // the class name
-     if(item.el.context.className == 'video-link') {
-       item.type = 'iframe';
-     } else {
-       item.type = 'image';
-     }
-        }
-      },
-  });
-});
