3f5b8ea98ab7de45eecb2ea14aefc66198c862b1
IshMSahni <ishsahni@hotmail.com>
2019-03-18 01:47:30 -0400

    maybe this works

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,18 +41,30 @@
 
    // Magnific popup calls
   $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        /*titleSrc: function(item) {
+          return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+        }*/
+      },
+      callbacks: {
+        elementParse: function(item) {
+     // the class name
+     if(item.el.context.className == 'video-link') {
+       item.type = 'iframe';
+     } else {
+       item.type = 'image';
+     }
+        }
+      },
   });
-
-})(jQuery); // End of use strict
+});
